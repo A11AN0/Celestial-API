caeecb67804be9628570cb554bfdd39de811dad6
Allan <matrixboston7613@gmail.com>
2022-01-25 14:57:24 +0800

    server successfully returns appropriate json object after request

--- server.js
+++ server.js
@@ -4,31 +4,39 @@ const app = express()
 
 app.use(cors())
 
-let celestialBodies = {
-    'Jupiter': {
+let celestialBodyList = {
+    'jupiter': {
         'Distance from Sun': '778.5 million km',
         'Orbital Period': '12 Years',
         'Radius': '69,111km',
         'Length of Day': '0d 9h 56m'
     },
-    'Neptune': {
+    'neptune': {
         'Distance from Sun': ' 4.495 billion km',
         'Orbital Period': '165 years',
         'Radius': '24,622 km',
         'Length of Day': '0d 16h 6m'
     },
-    'Mars': {
+    'mars': {
         'Distance from Sun': '227.9 million km',
         'Orbital Period': '687 days',
         'Radius': '3,389.5 km',
         'Length of Day': '1d 0h 37m'
     },
-    'Earth': {
-        'Distance from Sun': '227.9 million km',
-        'Orbital Period': '687 days',
-        'Radius': '3,389.5 km',
-        'Length of Day': '1d 0h 37m'
+    'earth': {
+        'Distance from Sun': '149.6 million km',
+        'Orbital Period': '365 days',
+        'Radius': '6,371 km',
+        'Length of Day': '1d 0h 37m',
     },
+
+    'unknown': {
+        'Distance from Sun': 'unknown',
+        'Orbital Period': 'unknown',
+        'Radius': 'unknown',
+        'Length of Day': 'unknown'
+    }
+    
 
 }
 
@@ -37,5 +45,14 @@ app.listen(9000, ()=>{
 })
 
 app.use(express.static("public"))
+
+app.get('/api/celestialBodyList/:celestialBodyName', (request, response)=>{
+    const celestialBody = request.params.celestialBodyName.toLowerCase()
+    if(celestialBodyList[celestialBody]){
+        response.json(celestialBodyList[celestialBody])
+    }else {
+        response.json(celestialBodyList['unknown'])
+    }       
+})
 
 
