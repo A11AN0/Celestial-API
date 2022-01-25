79d4c9a24566d2cc52e680f1c7a1bf7117f40e99
Allan <matrixboston7613@gmail.com>
2022-01-25 19:31:31 +0800

    input is detecting enter key pressed

--- client-side/main.js
+++ client-side/main.js
@@ -16,3 +16,22 @@ input.addEventListener('mouseout', ()=>{
     input.style.display="none"
 })
 
+const obtainCelestialBody = async () => {
+    const searchTerm = input.value
+    try{
+        const res = await fetch(`https://localhost:9000/api/celestialBodyList/${searchTerm}`)
+        const data = await res.json()
+        console.log(data)
+    }catch(err){
+        console.log(err)
+    }
+}
+
+
+input.addEventListener('focus', ()=>{
+    input.addEventListener('keydown', (event)=>{
+        if(event.key === 'Enter'){
+            console.log('hi')
+        }
+    })
+})
